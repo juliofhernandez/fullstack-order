065c7198f2ddbdb6b57a49ea4949995e3eff93ce
juliofhernandez <juliofernando1c@gmail.com>
2025-07-31 15:58:22 +0200

    init

---
+++ src/main/java/com/juliofhernandez/fullstackorder/FullstackOrderApplication.java
@@ -0,0 +1,13 @@
+package com.juliofhernandez.fullstackorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juliofhernandez/fullstackorder/FullstackOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliofhernandez.fullstackorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
